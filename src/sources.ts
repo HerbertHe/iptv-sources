0503bf390ac8deaa844473cdfb2218ebeb519570
HerbertHe <Herbert.He0229@gmail.com>
2023-12-13 10:39:57 +0800

    feat :sparkles: : support `iptv.org` stream source

--- src/sources.ts
+++ src/sources.ts
@@ -67,6 +67,54 @@ export const iptv_org_filter = (raw: string): [string, number] => {
     return [result.join("\n"), result.length - 1]
 }
 
+export const iptv_org_stream_filter = (raw: string): [string, number] => {
+    const rawArray = raw.split("\n")
+    const regExp = /\#EXTINF:-1\s+tvg\-id\=\"([^"]*)\",(.*)/
+    const invalidExp = /\#EXTVLCOPT:/
+
+    let i = 1
+    let sourced: string[] = []
+    let commented: string[] = []
+    let result = [rawArray[0]]
+
+    while (i < rawArray.length) {
+        const reg = regExp.exec(rawArray[i]) as RegExpExecArray
+        const invalid = invalidExp.test(rawArray[i + 1])
+
+        if (!!reg && !invalid) {
+            if (!reg[1]) {
+                // 没有 tvg-id
+                // 处理台标重复
+                if (!commented.includes(reg[2])) {
+                    const comment = reg[2].trim()
+                    commented.push(comment)
+
+                    result.push(
+                        rawArray[i]
+                            .replace(/\@\@[0-9]*/g, "")
+                            .replace(/\[([\s\S]*)\]/g, "")
+                            .trim()
+                    )
+                    result.push(rawArray[i + 1])
+                }
+            } else if (!sourced.includes(reg[1])) {
+                sourced.push(reg[1])
+                result.push(
+                    rawArray[i]
+                        .replace(/\@\@[0-9]*/g, "")
+                        .replace(/\[([\s\S]*)\]/g, "")
+                        .trim()
+                )
+                result.push(rawArray[i + 1])
+            }
+        }
+
+        i += 2
+    }
+
+    return [result.join("\n"), result.length - 1]
+}
+
 const epg_pw_sources: TSources = [
     {
         name: "China",
@@ -110,4 +158,35 @@ const iptv_org_sources: TSources = [
     },
 ]
 
-export const sources = [...epg_pw_sources, ...iptv_org_sources]
+const iptv_org_stream_sources: TSources = [
+    {
+        name: "iptv.org stream China",
+        f_name: "o_s_cn",
+        url: "https://fastly.jsdelivr.net/gh/iptv-org/iptv@master/streams/cn.m3u",
+        filter: iptv_org_stream_filter,
+    },
+    {
+        name: "iptv.org stream China 112114",
+        f_name: "o_s_cn_112114",
+        url: "https://fastly.jsdelivr.net/gh/iptv-org/iptv@master/streams/cn_112114.m3u",
+        filter: iptv_org_stream_filter,
+    },
+    {
+        name: "iptv.org stream China CCTV",
+        f_name: "o_s_cn_cctv",
+        url: "https://fastly.jsdelivr.net/gh/iptv-org/iptv@master/streams/cn_cctv.m3u",
+        filter: iptv_org_stream_filter,
+    },
+    {
+        name: "iptv.org stream China CGTN",
+        f_name: "o_s_cn_cgtn",
+        url: "https://fastly.jsdelivr.net/gh/iptv-org/iptv@master/streams/cn_cgtn.m3u",
+        filter: iptv_org_stream_filter,
+    },
+]
+
+export const sources = [
+    ...epg_pw_sources,
+    ...iptv_org_sources,
+    ...iptv_org_stream_sources,
+]
