5f47cf2e01dc5f343c9273ba865acd2117cd77b2
HerbertHe <Herbert.He0229@gmail.com>
2023-12-14 11:28:08 +0800

    fix :bug: : fix vercel

--- src/vercel.ts
+++ src/vercel.ts
@@ -10,7 +10,6 @@ interface IVercelJson {
         source: string
         destination: string
     }[]
-    outputDirectory: string
     buildCommand: string
 }
 
@@ -19,7 +18,6 @@ const updateVercelJson = (domains: string[]) => {
     const vercel: IVercelJson = {
         version: 2,
         rewrites: [],
-        outputDirectory: "m3u",
         buildCommand: "npm run build:vercel",
     }
 
