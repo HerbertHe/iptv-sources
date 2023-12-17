1ef61c8ff1de58ba2a428a6c6ab5876e086be83b
HerbertHe <Herbert.He0229@gmail.com>
2023-12-17 12:40:40 +0800

    fix :bug: : fix `iptv-sources.sh`

--- iptv-sources.sh
+++ iptv-sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # 定时构建命令
-docker stop iptv-sources && docker pull herberthe0229/iptv-sources:latest && docker run --name iptv-sources -p 3000:8080 -d herberthe0229/iptv-sources:latest
+docker stop iptv-sources && docker rm iptv-sources && docker pull herberthe0229/iptv-sources:latest && docker run --name iptv-sources -p 3000:8080 -d herberthe0229/iptv-sources:latest
