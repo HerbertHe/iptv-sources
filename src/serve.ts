ecee6fc53adddcf8ee0cb7151fbd6f10f4a17a5a
HerbertHe <Herbert.He0229@gmail.com>
2023-12-18 17:07:11 +0800

    perf :zap: : move static server from "live-server" to "koa"

---
+++ src/serve.ts
@@ -0,0 +1,41 @@
+import fs from "fs"
+import path from "path"
+
+import Koa from "koa"
+import { default as Static } from "koa-static"
+import Router from "koa-router"
+import MarkdownIt from "markdown-it"
+
+const app = new Koa()
+const router = new Router()
+
+app.use(Static("./m3u"))
+
+router.get("/", (ctx) => {
+    const markdown = fs
+        .readFileSync(path.resolve("m3u", "README.md"))
+        .toString()
+    const md = new MarkdownIt({ html: true })
+    ctx.body = `
+    <html lang="en">
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.5.0/github-markdown.css" integrity="sha512-LX/J+iRwkfRqaipVsfmi2B1S7xrqXNHdTb6o4tWe2Ex+//EN3ifknyLIbX5f+kC31zEKHon5l9HDEwTQR1H8cg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        body {
+            padding: 50px;
+        }
+    </style>
+    </head>
+    <body>
+        <div class="markdown-body">${md.render(markdown)}</div>
+    </body>
+    </html>
+    `
+})
+
+app.use(router.routes())
+
+app.listen(8080, () => {
+    console.log("Serving at http://127.0.0.1:8080")
+})
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup"
 
 export const tsup = defineConfig({
-    entry: ["src/index.ts"],
+    entry: ["src/index.ts", "src/serve.ts"],
     outDir: "dist",
     clean: true,
     format: ["esm"],
