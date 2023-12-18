6e5a4849339c17f5b3b72daa424d7115b1d81100
HerbertHe <Herbert.He0229@gmail.com>
2023-12-18 20:48:34 +0800

    fix :bug: : dark mode styles

--- src/serve.ts
+++ src/serve.ts
@@ -22,8 +22,16 @@ router.get("/", (ctx) => {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.5.0/github-markdown.css" integrity="sha512-LX/J+iRwkfRqaipVsfmi2B1S7xrqXNHdTb6o4tWe2Ex+//EN3ifknyLIbX5f+kC31zEKHon5l9HDEwTQR1H8cg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
-        body {
-            padding: 50px;
+        html, body {
+            margin: 0;
+            padding: 0;
+        }
+        .markdown-body {
+            padding: 50px 100px;
+        }
+
+        tr, td {
+            color: var(--color-fg-default);
         }
     </style>
     </head>
