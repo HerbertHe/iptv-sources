c4cdb853a6e0a981f1065b1155c1405c07747fa6
HerbertHe <Herbert.He0229@gmail.com>
2023-12-20 13:42:22 +0800

    feat :sparkles: : support epg sources `51zmt.top`

--- src/epgs/index.ts
+++ src/epgs/index.ts
@@ -10,6 +10,19 @@ export const epgs_sources: TEPGSource[] = [
         name: "fanmingming/live",
         f_name: "fmml",
         url: "https://fastly.jsdelivr.net/gh/fanmingming/live@main/e.xml"
+    },
+    {
+        name: "51zmt.top",
+        f_name: "51zmt",
+        url: "http://epg.51zmt.top:8000/e.xml"
+    },{
+        name: "51zmt.top cc",
+        f_name: "51zmt_cc",
+        url: "http://epg.51zmt.top:8000/cc.xml"
+    },{
+        name: "51zmt.top difang",
+        f_name: "51zmt_df",
+        url: "http://epg.51zmt.top:8000/difang.xml"
     }
 ]
 
