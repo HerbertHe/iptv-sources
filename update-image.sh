913da3e2ed60accde71d60a011206bf25101cacb
HerbertHe <Herbert.He0229@gmail.com>
2023-12-21 08:57:26 +0800

    fix :bug: : fix script

--- update-image.sh
+++ update-image.sh
@@ -4,7 +4,7 @@
 docker stop iptv-sources && docker rm iptv-sources && docker pull herberthe0229/iptv-sources:latest && docker run --name iptv-sources -p 3000:8080 -d herberthe0229/iptv-sources:latest
 
 # 获取历史版本
-UNUSED_IMAGES = $(docker images -q herberthe0229/iptv-sources --filter "dangling=true")
+UNUSED_IMAGES=$(docker images -q herberthe0229/iptv-sources --filter "dangling=true")
 
 # 删除历史版本
 if [ -n "$UNUSED_IMAGES" ];
