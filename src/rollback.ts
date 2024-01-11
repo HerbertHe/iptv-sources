e5f2c89d204c7d28266f3b9d3e448f5d7ebf6e45
HerbertHe <Herbert.He0229@gmail.com>
2024-01-11 12:29:32 +0800

    feat :sparkles: : support rollback from multi-sources

--- src/rollback.ts
+++ src/rollback.ts
@@ -1,28 +1,48 @@
 import { hrtime } from "process"
 
 import { ISource, filter } from "./sources"
+import { get_rollback_urls } from "./utils"
 
-// TODO 支持多渠道回滚，进行闭包递归调用
 export const updateByRollback = async (
     sr: ISource,
-    sr_filter?: (raw: string) => [string, number]
+    sr_filter?: (raw: string) => [string, number],
+    idx: number = 0
 ): Promise<[string, number] | undefined> => {
-    const now = hrtime.bigint()
-    const res = await fetch(`https://m3u.ibert.me/${sr.f_name}.m3u`)
-    const status = res.status
-    if (/^[2]/.test(status.toString())) {
-        console.log(
-            `Fetch m3u from ${
-                sr.name
-            } with ROLLBACK from https://m3u.ibert.me finished, cost ${
-                (parseInt(hrtime.bigint().toString()) -
-                    parseInt(now.toString())) /
-                10e6
-            } ms`
-        )
-        const text = await res.text()
-        return !!sr_filter ? sr_filter(text as string) : filter(text as string)
-    } else {
+    const rollback_urls = get_rollback_urls()
+    try {
+        const now = hrtime.bigint()
+        const res = await fetch(`${rollback_urls[idx]}/${sr.f_name}.m3u`)
+        const status = res.status
+        if (/^[2]/.test(status.toString())) {
+            console.log(
+                `Fetch m3u from ${sr.name} with ROLLBACK from ${
+                    rollback_urls[idx]
+                } finished, cost ${
+                    (parseInt(hrtime.bigint().toString()) -
+                        parseInt(now.toString())) /
+                    10e6
+                } ms`
+            )
+            const text = await res.text()
+            return !!sr_filter
+                ? sr_filter(text as string)
+                : filter(text as string)
+        }
+
+        console.log(`Fetch Rollback from ${rollback_urls[idx]} FAILED, try next...`)
+        if (++idx < rollback_urls.length) {
+            return updateByRollback(sr, sr_filter, idx)
+        }
+
+        console.log(`All failed, give up ${sr.name}!`)
+        return undefined
+    } catch (e) {
+        console.log(`Fetch Rollback from ${rollback_urls[idx]} FAILED, try next...`)
+        if (++idx < rollback_urls.length) {
+            return updateByRollback(sr, sr_filter, idx)
+        }
+
+        console.log(`All failed, give up ${sr.name}!`)
         return undefined
     }
 }
