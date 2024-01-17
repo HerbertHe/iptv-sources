04f26d5515fc624c657f89edb8a187c8ac6fd73b
HerbertHe <Herbert.He0229@gmail.com>
2024-01-17 11:41:50 +0800

    fix :bug: : update

--- src/utils/logo.ts
+++ src/utils/logo.ts
@@ -3,4 +3,8 @@ import { channels_logo } from "./const"
 const is_fmml_logo_channel = (c: string) => channels_logo.includes(c)
 
 export const with_fmml_logo_channel = (c: string) =>
-    is_fmml_logo_channel(c) ? `https://live.fanmingming.com/tv/${c}.png` : void 0
+    is_fmml_logo_channel(c)
+        ? `https://live.fanmingming.com/tv/${c}.png`
+        : is_fmml_logo_channel(c.toLowerCase())
+        ? `https://live.fanmingming.com/tv/${c.toLowerCase()}.png`
+        : void 0
