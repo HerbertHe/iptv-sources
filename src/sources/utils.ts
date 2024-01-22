f89cb053684e89273de046ff934e6cb820f8cc3c
HerbertHe <Herbert.He0229@gmail.com>
2024-01-22 16:07:31 +0800

    fix :bug: : fix

--- src/sources/utils.ts
+++ src/sources/utils.ts
@@ -31,29 +31,3 @@ export const with_github_raw_url_proxy = (u: string) => {
         ? u
         : `${get_github_raw_proxy_url()}/${u}`
 }
-
-export const get_source_id = (desc: string, lower: boolean = false): string => {
-    const regExp = /#EXTINF:-1\s+([^,]*),([^,]+)/
-    const tvgIDExp = /tvg-id="([^"]+)"/
-    const tvgNameExp = /tvg-name="([^"]+)"/
-
-    const [, rawTvg, rawName] = regExp.exec(desc) as RegExpExecArray
-    if (!!rawTvg && tvgIDExp.test(rawTvg)) {
-        const id = tvgIDExp.exec(rawTvg)![1]
-        return lower ? id.toLowerCase() : id
-    }
-
-    if (!!rawTvg && tvgNameExp.test(rawTvg)) {
-        const name = tvgNameExp.exec(rawTvg)![1]
-        return lower ? name.toLowerCase() : name
-    }
-
-    const name = rawName
-        .replace(/\[([^\]]+)\]/g, "")
-        .replace(/\(([^\)]+)\)/g, "")
-        .replace(/「([^」]+)」/g, "")
-        .replace(/\-/g, "")
-        .replace(/ +/g, "")
-
-    return lower ? name.toLowerCase() : name
-}
--- src/utils/id.ts
+++ src/utils/id.ts
@@ -1,12 +1,16 @@
+import { converter } from "../sources/utils"
+
 export const get_channel_id = (extinf: string) => {
     const regExp = /\#EXTINF:-1([^,]*),(.*)/
-    const name = regExp.exec(extinf)![2]
+    const name = converter(regExp.exec(extinf)![2])
 
     return name
         .replace(/\[([^\]]*)\]/g, "")
         .replace(/\(([^\)]*)\)/g, "")
+        .replace(/「([^」]+)」/g, "")
         .replace(/\-/g, "")
         .replace(/\@\@[0-9]*/g, "")
+        .replace(/Ⅰ/g, "")
         .replace(/ +/g, "")
         .trim()
 }
