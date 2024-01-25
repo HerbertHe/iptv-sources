19d5b64a26c2b223762648ca60d830e2fe76c2b9
HerbertHe <Herbert.He0229@gmail.com>
2024-01-26 01:17:30 +0800

    fix :bug: : update

--- src/sources/fanmingming_live.ts
+++ src/sources/fanmingming_live.ts
@@ -19,19 +19,19 @@ export const fanmingming_live_filter: ISource["filter"] = (
 
 export const fanmingming_live_sources: TSources = [
     {
-        name: "fanmingming/live domainv6",
-        f_name: "fmml_dv6",
-        url: "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/domainv6.m3u",
-        filter: fanmingming_live_filter,
-    },
-    {
         name: "fanmingming/live ipv6",
         f_name: "fmml_ipv6",
         url: "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u",
         filter: fanmingming_live_filter,
     },
     {
-        name: "fanmingming/live v6",
+        name: "fanmingming/live domainv6(Invalid)",
+        f_name: "fmml_dv6",
+        url: "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/Invalid/domainv6.m3u",
+        filter: fanmingming_live_filter,
+    },
+    {
+        name: "~~fanmingming/live v6~~(Backup Only)",
         f_name: "fmml_v6",
         url: "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/v6.m3u",
         filter: fanmingming_live_filter,
