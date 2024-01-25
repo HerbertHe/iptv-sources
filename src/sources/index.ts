2bcc5d8f4cf10c8c1ac83ee83747c73aa58a5e5a
HerbertHe <Herbert.He0229@gmail.com>
2024-01-26 02:02:26 +0800

    feat :sparkles: : support source `joevess/IPTV`

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -4,6 +4,7 @@ export * from "./yang_m3u"
 export * from "./yuechan_live"
 export * from "./fanmingming_live"
 export * from "./qwerttvv_bj_iptv"
+export * from "./joevess_iptv"
 export * from "./utils"
 
 import {
@@ -14,12 +15,14 @@ import {
     yuechan_live_sources,
     fanmingming_live_sources,
     qwerttvv_bj_iptv_sources,
+    joevess_iptv_sources
 } from "."
 
 export const sources = [
     ...fanmingming_live_sources,
     ...yuechan_live_sources,
     ...yang_m3u_sources,
+    ...joevess_iptv_sources,
     ...iptv_org_sources,
     ...iptv_org_stream_sources,
     ...epg_pw_sources,
---
+++ src/sources/joevess_iptv.ts
@@ -0,0 +1,35 @@
+import { collectM3uSource } from "../utils"
+import { handle_m3u, ISource, type TSources } from "./utils"
+
+export const joevess_iptv_filter: ISource["filter"] = (
+    raw,
+    caller,
+    collectFn
+): [string, number] => {
+    const rawArray = handle_m3u(raw)
+
+    rawArray.unshift("#EXTM3U")
+
+    if (caller === "normal" && collectFn) {
+        for (let i = 1; i < rawArray.length; i += 2) {
+            collectM3uSource(rawArray[i], rawArray[i + 1], collectFn)
+        }
+    }
+
+    return [rawArray.join("\n"), (rawArray.length - 1) / 2]
+}
+
+export const joevess_iptv_sources: TSources = [
+    {
+        name: "joevess/IPTV home",
+        f_name: "j_home",
+        url: "https://raw.githubusercontent.com/joevess/IPTV/main/home.m3u8",
+        filter: joevess_iptv_filter,
+    },
+    {
+        name: "joevess/IPTV iptv",
+        f_name: "j_iptv",
+        url: "https://raw.githubusercontent.com/joevess/IPTV/main/iptv.m3u8",
+        filter: joevess_iptv_filter,
+    },
+]
